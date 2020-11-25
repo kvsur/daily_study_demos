b634322d7ae80773937fba3ce9598b9ca43d49c3
kvsur <hello1024lc@gmail.com>
2020-11-25 21:47:09 +0800

    @udpate high orfer funciton

--- JS/高阶函数/compose.js
+++ JS/高阶函数/compose.js
@@ -1,18 +1,19 @@
 function compose(middleware) {
     return function (context, next) {
-        let index = -1;
+        let index = -1; // 定义当前将要调用的middlewar中的index
         return dispatch(0);
 
         function dispatch(i) {
-            if (i <= index)
+            if (i <= index) // outofbound
                 return Promise.reject(new Error('next() called multiple times'));
 
             index = i;
             let fn = middleware[i];
-            if (i === middleware.length) fn = next;
-            if (!fn) return Promise.resolve();
+            if (i === middleware.length) fn = next; // 获取当前middleware
+            if (!fn) return Promise.resolve(); // fn不存在时直接返回即可
 
             try {
+                // 调用当前 middleware ，next = dispatch.bind(null, i+1) -> middleware 的下一个
                 return Promise.resolve(fn(context, dispatch.bind(null, i + 1)));
             } catch (err) {
                 return Promise.reject(err);
