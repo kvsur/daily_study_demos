7ff601919b12c6c931d09f13f8cacf54e7ab090a
kvsur <hello1024lc@gmail.com>
2020-12-17 17:00:19 +0800

    Reduc DIY by TypeScript

--- Redux/lib/bindActionCreators.js
+++ Redux/lib/bindActionCreators.js
@@ -1,34 +1,33 @@
-import { ActionCreator, ActionCreators, BoundActionCreators, Dispatch } from "./define";
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.bindActionCreators = void 0;
 /**
  * 通过闭包的方式隐藏actionCreator和dispatch
  */
-function bindActionCreator(actionCreator: ActionCreator, dispatch: Dispatch) {
-    return function() {
+function bindActionCreator(actionCreator, dispatch) {
+    return function () {
         /** 这里this取决于执行环境，在哪里使用就会指向当前的this */
         return dispatch(actionCreator.apply(this, arguments));
-    }
+    };
 }
 /**
  * 第一个参数要么是一个action 创建函数，要么是一个素的对象且key对应的value是一个 ActionCreator
  */
-export function bindActionCreators(actionCreators: ActionCreator | ActionCreators, dispatch: Dispatch) {
+function bindActionCreators(actionCreators, dispatch) {
     if (typeof actionCreators === 'function') {
         return bindActionCreator(actionCreators, dispatch);
     }
-
     if (typeof actionCreators !== 'object' || actionCreators === null) {
         throw new Error('ActionCreator must be plain and value type is a function can return an action, or just a function can return an action');
     }
-
-    const actionCreatorKeys = Object.keys(actionCreators);
-    const boundActionCreators: BoundActionCreators = {};
-
-    actionCreatorKeys.forEach(key => {
-        const actionCreator = actionCreators[key];
+    var actionCreatorKeys = Object.keys(actionCreators);
+    var boundActionCreators = {};
+    actionCreatorKeys.forEach(function (key) {
+        var actionCreator = actionCreators[key];
         if (typeof actionCreator === 'function') {
             boundActionCreators[key] = bindActionCreator(actionCreator, dispatch);
         }
     });
-
     return boundActionCreators;
 }
+exports.bindActionCreators = bindActionCreators;
--- Redux/lib/createStore.js
+++ Redux/lib/createStore.js
@@ -1,12 +1,13 @@
-import { CreateStore, Store, RCSF, Fn, Action, State, Reducer } from "./define";
-
-export const createStore: CreateStore = function (reducer ,initState, rewriteCreateStoreFn): Store {
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.createStore = void 0;
+var createStore = function (reducer, initState, rewriteCreateStoreFn) {
     /**
      * 如果没有传入initState 且需要重写createStore时，第二个参数就是 rewriteCreateStoreFn
      * @usage createStore(reducer, rewriteCreateStoreFn)
      */
     if (typeof initState === 'function') {
-        rewriteCreateStoreFn = <RCSF>initState;
+        rewriteCreateStoreFn = initState;
         initState = undefined;
     }
     /**
@@ -14,48 +15,40 @@ export const createStore: CreateStore = function (reducer ,initState, rewriteCre
      * 则直接使用 rewriteCreateStoreFn 生成的 createStore
      */
     if (rewriteCreateStoreFn) {
-        const newCreateStore = rewriteCreateStoreFn(createStore);
+        var newCreateStore = rewriteCreateStoreFn(exports.createStore);
         return newCreateStore(reducer, initState);
     }
-
-    let state = initState;
-    let listeners: Fn[] = [];
-
+    var state = initState;
+    var listeners = [];
     /** 订阅 */
-    function subscribe(listener: Fn) {
+    function subscribe(listener) {
         listeners.push(listener);
-
         /** 返回取消订阅的函数 */
-        return function() {
-            const index = listeners.indexOf(listener);
+        return function () {
+            var index = listeners.indexOf(listener);
             listeners.splice(index, 1);
-        }
+        };
     }
-
-    function dispatch(action: Action) {
-        state = reducer(state, action)
-
-        for (let i = 0; i < listeners.length; i++) {
-            const listener = listeners[i];
+    function dispatch(action) {
+        state = reducer(state, action);
+        for (var i = 0; i < listeners.length; i++) {
+            var listener = listeners[i];
             listener();
         }
     }
-
-    function getState(): State {
+    function getState() {
         return state;
     }
-
-    function replaceReducer(newReducer: Reducer) {
+    function replaceReducer(newReducer) {
         reducer = newReducer;
         dispatch({ type: Symbol() });
     }
-
     dispatch({ type: Symbol() });
-
     return {
-        subscribe,
-        getState,
-        dispatch,
-        replaceReducer
+        subscribe: subscribe,
+        getState: getState,
+        dispatch: dispatch,
+        replaceReducer: replaceReducer
     };
-}
+};
+exports.createStore = createStore;
