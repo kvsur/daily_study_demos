aaa7432e1bc8643339d94c519cea0fc204048bc3
kvsur <hello1024lc@gmail.com>
2020-12-17 17:10:41 +0800

    Redux DIY by TypeScript

---
+++ Redux/index.ts
@@ -0,0 +1,4 @@
+export { bindActionCreators } from './bindActionCreators';
+export { applyMiddlewares } from './applyMiddlewares';
+export { createStore } from './createStore';
+export { combineReducers } from './combineReducers';
