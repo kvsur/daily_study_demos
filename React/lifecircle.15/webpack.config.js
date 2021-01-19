cb6d1f3774035d74991f289e4938546f7e87d1ad
kvsur <hello1024lc@gmail.com>
2021-01-19 22:30:44 +0800

    Test difference React version lifecircle

---
+++ React/lifecircle.15/webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+module.exports = {
+    mode: 'development',
+    entry: {
+        main: './src/main.js'
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /^.*js$/,
+                loader: 'babel-loader'
+            }
+        ]
+    }
+}
